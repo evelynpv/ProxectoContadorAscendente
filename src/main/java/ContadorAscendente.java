f19b9fc229b1c988fbdfca97e38b504911f2efd0
Evelyn Piñeyro <a21evelynpv@iessanclemente.net>
2022-03-03 23:32:22 +0100

    Xerouse o código estándar da clase ContadorAscendente

--- src/main/java/ContadorAscendente.java
+++ src/main/java/ContadorAscendente.java
@@ -1,2 +1,23 @@
 public class ContadorAscendente {
+
+    int valorActual;
+
+    public ContadorAscendente(int valorActual) {
+        this.valorActual = valorActual;
+    }
+
+    public int getValorActual() {
+        return valorActual;
+    }
+
+    public void setValorActual(int valorActual) {
+        this.valorActual = valorActual;
+    }
+
+    @Override
+    public String toString() {
+        return "ContadorAscendente{" +
+                "valorActual=" + valorActual +
+                '}';
+    }
 }
